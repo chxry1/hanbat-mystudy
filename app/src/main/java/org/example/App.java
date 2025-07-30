6736935292d73de782a81d9a94f9fd4735e0d50f
chxry1 <xhtmrka1@gmail.com>
2025-07-30 15:01:34 +0900

    Intellij 빌드

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,19 @@
 package org.example;
 
-public class App {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class  App {
   public String getGreeting() {
     return "Hello World!";
   }
 
   public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("이름? ");
+    String name = scanner.nextLine();
+    System.out.printf("안녕하세요 %s 님!\n", name);
+    scanner.close();
   }
 }
