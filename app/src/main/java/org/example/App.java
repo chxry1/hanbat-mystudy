a317fe75b748f2f98e8ee87d35468f0c35e9f9da
chxry1 <xhtmrka1@gmail.com>
2025-07-31 13:17:38 +0900

    SpringBoot server start

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,35 +1,19 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-
+@SpringBootApplication
 public class  App {
   public String getGreeting() {
     return "Hello World!";
   }
 
   public static void main(String[] args) {
-    class Calculator {
-      int result;
-      void plus(int a) {
-        this.result += a;
-      }
-      void minus(int a) {
-        this.result -= a;
-      }
-    }
-
-    Calculator c1 = new Calculator();
-    Calculator c2 = new Calculator();
-
-    c1.plus(100);
-    c2.plus(555);
-
-//    Scanner scanner = new Scanner(System.in);
-//    System.out.print("이름? ");
-//    String name = scanner.nextLine();
-//    System.out.printf("안녕하세요 %s 님!\n", name);
-//    scanner.close();
+    SpringApplication.run(Appconfig.class, args);
+    System.out.println("스프링부트 서버 시작!");
   }
 }
---
+++ app/src/main/java/org/example/Appconfig.java
@@ -0,0 +1,5 @@
+package org.example;
+
+//스프링부 애플리케이션을 실행하는데 필요한 설정을 담고 있는 클래스이다.
+public class Appconfig {
+}
